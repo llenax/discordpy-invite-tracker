ee872f6ff869aeac7c86023ba94d62f9e68e5444
llenax <67596732+llenax@users.noreply.github.com>
2021-03-26 19:51:59 +0300

    remove log

--- client.py
+++ client.py
@@ -22,28 +22,18 @@ SOFTWARE.
 import discord
 from discord.ext import commands
 
-invites_dict = {}
+invites_dict = {} # Store
 
-log = 813158187921965106  # YOUR CHANNEL ID
+token = "NzYyMzI5OTc1NDY2MzYwODUz.X3nlHQ.4Tz-nSgkdClOh9WeZoP23RqEojs"
+prefix = "!"
 
-token = "YOUR TOKEN"
-
-client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-
-
-async def set_invites(guild):
-    guild_invites = await guild.invites()
-    temp = [tuple((invite.code, invite.uses)) for invite in guild_invites]
-    invites_dict[guild.id] = temp
-
+client = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
     for guild in client.guilds:
         await set_invites(guild)
-    print("online")
-
-
+    
 @client.event
 async def on_invite_create(invite):
     await set_invites(invite.guild)
@@ -56,37 +46,25 @@ async def on_invite_delete(invite):
 
 @client.event
 async def on_member_join(member):
-    guild = member.guild
-    log_channel = discord.utils.get(member.guild.channels, id=log)
-    embed = discord.Embed(color=discord.Color.blurple())
     if not member.bot:
         global invites_dict
+        guild = member.guild
         guild_invites = await guild.invites()
-
         for invite in guild_invites:
             try:
                 for invite_dict in invites_dict.get(guild.id):
                     if invite_dict[0] == invite.code:
                         if int(invite.uses) > invite_dict[1]:
-                            if log_channel is not None:
-                                embed.description = f"{member.mention} joined server. Invited by {invite.inviter.mention}"
-                                embed.add_field(
-                                    name="Used Invite",
-                                    value=
-                                    f"Inviter: (`{invite.inviter.name}#{invite.inviter.discriminator}`) | `{invite.inviter.id}`\nHave **{invite.uses}** uses."
-                                )
-                                await log_channel.send(embed=embed)
-            except:  #exception as e:
-                pass  #print(e)
+                            print(f"{member} joined. Invited with: {invite.code} | by {invite.inviter}")
+            except Exception as error:
+                print(error)
     else:
-        embed.description = f"{member.mention} joined server and its a bot."
-        await log_channel.send(embed=embed)
+        print("Someone joined server. It could be a bot.")
 
 
-@client.event
-async def on_member_remove(member):
-    guild = member.guild
-    await set_invites(guild)
+async def set_invites(guild):
+    guild_invites = await guild.invites()
+    invites_dict[guild.id] = [tuple((invite.code, invite.uses)) for invite in guild_invites]
 
 
 client.run(token)
